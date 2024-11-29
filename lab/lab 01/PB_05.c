57ea1c14557184061bcf08f9f02cf0a0bd088c52
ponmak <peeranut.4498@gmail.com>
2024-11-29 21:18:26 +0700

    lab 01 done

--- lab/lab 01/PB_05.c
+++ lab/lab 01/PB_05.c
@@ -8,6 +8,18 @@ int main(){
     int twenety;
     int fifty;
 
+    // fifty
     fifty = amount / 50;
-    amount = amount - 
+    amount = amount - (50 * fifty);
+
+    // twenety
+    twenety = amount / 20;
+    amount = amount - (20 * twenety);
+
+    // five
+    five = amount / 5;
+    amount = amount - (5 * five);
+
+    // left is amount
+    printf("1: %d\n5: %d\n20: %d\n50: %d", amount, five, twenety, fifty);
 }
