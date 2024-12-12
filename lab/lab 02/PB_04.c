d7e40ca1a21b8fc6e2696264aecc5f1f8c4e54f4
ponmak <peeranut.4498@gmail.com>
2024-12-12 20:41:07 +0700

    lab 02 complete

--- lab/lab 02/PB_04.c
+++ lab/lab 02/PB_04.c
@@ -8,22 +8,22 @@ int main() {
 
     fgets(input_str, 10, stdin);
     float num = atof(input_str);
+    float cal_num = 0;
 
-    if (num <= 0){
-        printf("Error: Salary must be greater or equal to 0");
+    if (num < 0){
+        printf("Error: Salary must be greater or equal to 0\n");
     } else {
-        float cal_num = 0;
-        if (num <= 300000){
+        if (num <= 300000.00){
             cal_num += num * (5.0 / 100);
         } else {
-            cal_num += 300000 * (5.0 / 100);
+            cal_num += 300000.00 * (5.0 / 100);
             //printf("(!) now_cal %f\n", cal_num);
-            num -= 300000;
+            num -= 300000.00;
             cal_num += num * (10.0 /100);
             //printf("(!) now_cal %f\n", cal_num);
         }
 
-        printf("%.02f",cal_num);
+        printf("%.2f",cal_num);
     }
     
     return 0;
