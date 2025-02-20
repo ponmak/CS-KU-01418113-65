2983816ad981c8566eb645daa389ae14b0a716da
ponmak <peeranut.4498@gmail.com>
2025-02-20 21:09:18 +0700

    lab 08 done

--- lab/lab 08/PB_02.c
+++ lab/lab 08/PB_02.c
@@ -1,8 +1,54 @@
-// 02
+// 02 ค้นหาและใส่คะแนน
+
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 
-int main(int argc, char const *argv[]) {
+typedef struct {
+    char id[15];
+    int score;
+} studentRecord;
 
-    return 0;
+void init(studentRecord list[], int size) {
+    for (int i = 0; i < size; i++){
+        char __id[12];  
+        sprintf(__id, "5510000%d", i+1);  
+        strcpy(list[i].id, __id);
+        list[i].score = 0 ;
+    }
+}
+
+void enterScore(studentRecord list[], int size){
+    char input_id[15];
+    int input_score;
+
+    scanf("%s %d", input_id, &input_score);
+
+    for (int i = 0; i < size; i++) {
+        if (strstr(list[i].id, input_id) ) {  
+            list[i].score = input_score;  
+            break;
+        }
+    }
+}
+
+void printRecords(studentRecord list[], int size) {
+	int i;
+
+    printf("   ID        Score\n");
+    for (i = 0; i < size; i++) {
+	    printf("%s       %d\n", list[i].id, list[i].score);
+	}
+}
+
+
+
+
+int main() {
+	int enter;
+	studentRecord list[3];
+
+	init(list, 3);
+        enterScore(list, 3);
+	printRecords(list, 3);
+	return 0;
 }
--- lab/lab 08/PB_06.c
+++ lab/lab 08/PB_06.c
@@ -1,31 +1,53 @@
 // 06หาค่าผลบวกและผลคูณระหว่างจำนวนเชิงซ้อน 2 จำนวน
+
 #include <stdio.h>
 
 struct complex {
-  
-
+    float x;
+    float y;
 };
 
- add_complex( ){
+struct complex add_complex(struct complex f_complex, struct complex s_complex){
+    struct complex add_number;
 
+    add_number.x = f_complex.x + s_complex.x;
+    add_number.y = f_complex.y + s_complex.y;
+
+    return add_number;
 }
 
- multiply_complex( ){
+struct complex multiply_complex(struct complex f_complex, struct complex s_complex){
+    struct complex multiply_number;
 
+    multiply_number.x = (f_complex.x * s_complex.x) - (f_complex.y * s_complex.y);
+    multiply_number.y = (f_complex.x * s_complex.y) + (f_complex.y * s_complex.x);
+
+    return multiply_number;
 }
 
-void print_complex( ){
+void print_complex(char *string, struct complex f_complex){
+    printf("%s", string);
 
+    if (f_complex.y == 0) {
+        printf("%.1f", f_complex.x);  
+    } else if (f_complex.x == 0) {
+        printf("%.1fi", f_complex.y);  
+    } else {
+        printf("%.1f %c %.1fi", f_complex.x, (f_complex.y > 0 ? '+' : '-'), (f_complex.y > 0 ? f_complex.y : -f_complex.y));
+    }
+
+    printf("\n");
 }
 
 int main(){
   
-    // 
+    struct complex C,D,E,F;
     
     printf("Enter C: ");
-    scanf("%f %f", &C. , &C.);
-    //
-
+    scanf("%f %f", &C.x , &C.y);
+    
+    printf("Enter D: ");
+    scanf("%f %f", &D.x , &D.y);
 
     E = add_complex(C, D);
     F = multiply_complex(C, D);
